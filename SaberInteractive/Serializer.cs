c4194d4ee1d237b0639e2558bfb5bebaa9470370
valentin <valentin-42@mail.ru>
2018-10-31 22:19:59 +0300

    create new class Serializer

---
+++ SaberInteractive/Serializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace SaberInteractive
+{
+    public class Serializer
+    {
+        public void Serialize(FileStream s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
