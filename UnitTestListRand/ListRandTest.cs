baa3536568f02c79b2e418b3bc017918b707e77d
valentin <valentin-42@mail.ru>
2018-10-31 21:56:19 +0300

    creat tests

--- UnitTestListRand/ListRandTest.cs
+++ UnitTestListRand/ListRandTest.cs
@@ -1,14 +1,62 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
+using SaberInteractive;
 
 namespace UnitTestListRand
 {
     [TestFixture]
     public class ListRandTest
     {
-        [Test]
-        public void TestMethod1()
+        private ListRand _listRand;
+
+
+        [TestCase(5)]
+        [TestCase(0)]
+        [TestCase(100)]
+        public void AddItemsAndCheckCount(int count)
         {
+            _listRand = new ListRand();
+            Assert.IsTrue(_listRand.Count.Equals(0),
+                $"ListRand isn't empty");
+
+            var datas = Enumerable.Range(0, count).Select(_ => Guid.NewGuid().ToString()).ToList();
+
+            foreach (var data in datas)
+            {
+                _listRand.Add(data);
+            }
+
+            Assert.IsTrue(_listRand.Count.Equals(datas.Count()),
+                $"Counts not equals. ListRand {_listRand.Count}, expected {datas.Count()}");
+        }
+
+        [TestCase(5)]
+        [TestCase(0)]
+        [TestCase(100)]
+        public void EqualsItems(int count)
+        {
+            _listRand = new ListRand();
+            Assert.IsFalse(_listRand.ToList().Any(),
+                $"ListRand isn't empty");
+
+            var datas = Enumerable.Range(0, count).Select(_ => Guid.NewGuid().ToString()).ToList();
+
+            foreach (var data in datas)
+            {
+                _listRand.Add(data);
+            }
+
+            var listString = _listRand.ToList();
+            
+            Assert.IsTrue(listString.Count.Equals(datas.Count),
+                $"Counts not equals. ListRand {_listRand.Count}, expected count {datas.Count}");
+
+            for (int i = 0; i < datas.Count; i++)
+            {
+                Assert.IsTrue(listString[i].Equals(datas[i]),
+                    $"Items not equals. Actual '{listString[i]}', expected {datas[i]}");
+            }
         }
     }
 }
