0a86d8c246d714f78b67f5babc8eb058e24bd8fb
valentin <valentin-42@mail.ru>
2018-10-31 21:26:59 +0300

    add methods in ListRand
    add: Add() and ToList()

--- SaberInteractive/ListRand.cs
+++ SaberInteractive/ListRand.cs
@@ -9,9 +9,49 @@ namespace SaberInteractive
 {
     public class ListRand
     {
-        public ListNode Head;
-        public ListNode Tail;
-        public int Count;
+        private ListNode Head;
+        private ListNode Tail;
+
+        public int Count { get; private set; }
+
+        public void Add(string data)
+        {
+            var newNode = new ListNode() { Data = data };
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                Count++;
+                return;
+            }
+
+            if (Head == Tail)
+            {
+                Head.Next = newNode;
+                newNode.Perv = Head;
+                Tail = newNode;
+                Count++;
+                return;
+            }
+
+            newNode.Perv = Tail;
+            Tail.Next = newNode;
+            Tail = newNode;
+        }
+
+        public List<string> ToList(bool isReverse = false)
+        {
+            var result = new List<string>();
+
+            var current = isReverse ? Tail : Head;
+            while (current != null)
+            {
+                result.Add(current.Data);
+                current = isReverse ? current.Perv : current.Next;
+            }
+
+            return result;
+        }
 
         public void Serialize(FileStream s)
         {
