e3f31d24ed099ac418381f56341d8941e465d407
valentin <valentin-42@mail.ru>
2018-10-31 21:08:47 +0300

    add test project and install NUnit.Framework

---
+++ UnitTestListRand/ListRandTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestListRand
+{
+    [TestFixture]
+    public class ListRandTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
