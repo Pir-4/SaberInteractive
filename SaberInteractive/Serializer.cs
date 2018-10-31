61f780eb4c3a5f0a8921c5be6060ebe4a0fed5d5
valentin <valentin-42@mail.ru>
2018-10-31 22:32:09 +0300

    add mathods for help Serialize

--- SaberInteractive/Serializer.cs
+++ SaberInteractive/Serializer.cs
@@ -5,6 +5,12 @@ namespace SaberInteractive
 {
     public class Serializer
     {
+        private readonly Func<string, string> _packagingItem = item => $"[{item}]";
+
+        private readonly Func<string,string, string> _packagingProperty =
+            (name, value) => string.IsNullOrEmpty(value) ? string.Empty :
+                $"{name}:{value}";
+
         public void Serialize(FileStream s, ListNode startNode)
         {
             throw new NotImplementedException();
