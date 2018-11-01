d0c1169b792477cdd60709e19cf7860633a090a8
valentin <valentin-42@mail.ru>
2018-11-01 22:37:24 +0300

    add test class

---
+++ UnitTestListRand/SerializeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace UnitTestListRand
+{
+    [TestFixture()]
+   public class SerializeTests : ListRandTests
+    {
+
+        [Test]
+        public void Serialize()
+        {
+            var datas = InitDatasAndListRand(3);
+        }
+    }
+}
