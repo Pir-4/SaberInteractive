682fd232f41b39dd6baa449c8b2e68ca2db68349
valentin <valentin-42@mail.ru>
2018-11-01 22:32:38 +0300

    first realization Serialize

--- SaberInteractive/Serializer.cs
+++ SaberInteractive/Serializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 
 
@@ -8,28 +9,42 @@ namespace SaberInteractive
 {
     public class Serializer
     {
-        private readonly Func<string, string> _packagingItem = item => $"[{item}]";
+        private static Dictionary<ListNode, string> _nodeDictionary;
+        private static readonly Func<string, string> _packagingItem = item => $"[{item}]";
 
-        private readonly Func<string,string, string> _packagingProperty =
+        private static readonly Func<string, string, string> _packagingProperty =
             (name, value) => string.IsNullOrEmpty(value) ? string.Empty :
                 $"{name}:{value}";
 
-        private Dictionary<ListNode, string> _nodeDictionary;
-
-        public void Serialize(FileStream s, ListNode head)
+        public static void Serialize(FileStream s, ListNode head)
         {
             var buffer = new StringBuilder();
-            _nodeDictionary = new Dictionary<ListNode, string>();
             var current = head;
             while (current != null)
             {
-                
+                var currentBuffer = new StringBuilder();
+                foreach (PropertyInfo propertyInfo in current.GetType().GetProperties())
+                {
+                    var guid = GetNodeId(propertyInfo.GetValue(propertyInfo) as ListNode);
+                    currentBuffer.Append(_packagingProperty(propertyInfo.Name, guid));
+                }
+                buffer.Append(_packagingItem(currentBuffer.ToString()));
             }
         }
 
-        public ListNode Deserialize(FileStream s)
+        public static ListNode Deserialize(FileStream s)
         {
             throw new NotImplementedException();
+        }
+
+        private static string GetNodeId(ListNode node)
+        {
+            _nodeDictionary = _nodeDictionary ?? new Dictionary<ListNode, string>();
+            if (!_nodeDictionary.ContainsKey(node))
+            {
+                _nodeDictionary[node] = node.Guid.ToString();
+            }
+            return _nodeDictionary[node];
         }
     }
 }
