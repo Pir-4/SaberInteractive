bf06cfce4d4cf0def5b9959c8a84136ed85bd4ec
Agapitov Valentin <valentin.agapitov@kaspersky.com>
2018-11-02 10:57:28 +0300

    create fist version test

--- UnitTestListRand/SerializeTests.cs
+++ UnitTestListRand/SerializeTests.cs
@@ -13,15 +13,16 @@ namespace UnitTestListRand
     [TestFixture()]
    public class SerializeTests : ListRandTests
     {
-        private const string TestFolderPath = @"E:\education\programs\SaberInteractive\Tests";
+        private const string TestFolderPath = @"E:\Git\My\SaberInteractive\TestFiles";
         private readonly string _serializeFile = Path.Combine(TestFolderPath,"serialize.txt");
         private readonly string _deserializeFile = Path.Combine(TestFolderPath, "deserialize.txt");
 
-
-        [Test]
-        public void Serialize()
+        [TestCase(5)]
+        [TestCase(0)]
+        [TestCase(100)]
+        public void SerializeCountTest(int count)
         {
-            InitDatasAndListRand(3);
+            var datas = InitDatasAndListRand(count);
 
             if(File.Exists(_serializeFile))
                 File.Delete(_serializeFile);
@@ -29,6 +30,17 @@ namespace UnitTestListRand
             using (var fileStream = new FileStream(_serializeFile,FileMode.OpenOrCreate,FileAccess.Write))
             {
                 Serializer.Serialize(fileStream, _listRand.Head);
+            }
+
+            using (var reader = new StreamReader(_serializeFile))
+            {
+                foreach (var data in datas)
+                {
+                    var actualValue = reader.ReadLine();
+                    Assert.IsTrue(actualValue.Contains(data), 
+                        $"Serialize string not contains expected data." +
+                        $"Actual '{actualValue}', expected {data}");
+                }
             }
         }
     }
